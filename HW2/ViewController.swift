8a1ce80843bb6f31f14d2b4bd9fd5e1b68c9b38c
marti265 <marti265@rangers.uwp.edu>
2019-02-24 18:12:29 -0600

    Initial Commit

---
+++ HW2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW2
+//
+//  Created by Jose Martinez on 2/24/19.
+//  Copyright © 2019 Jose. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
